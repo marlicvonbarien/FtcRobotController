7e51408b012724cae943694c9d75e17f8cb05ace
marlicvonbarien <johnsnat2026@proton.me>
2026-09-15 19:50:28 -0400

    Pushing now

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPractice.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp
+public class TeamMemberPractice extends OpMode {
+    public void init(){
+
+    }
+
+    public void loop() {
+        
+    }
+}
